bf44b6c9ef030428c76ea035a93f2e34a4d6c16d
gabrielribeirogpx-arch <gabrielribeirogpx@gmail.com>
2026-03-17 23:40:51 -0300

    Resolve Alembic multiple heads

---
+++ Super_SaaS_ Burger_backend/alembic/versions/594f9e82b5d4_merge_multiple_heads.py
@@ -0,0 +1,21 @@
+"""merge multiple heads
+
+Revision ID: 594f9e82b5d4
+Revises: 0030_loyalty_reais_por_ponto, 0030_order_destination_coordinates
+Create Date: 2025-08-08 00:00:00.000000
+"""
+
+from __future__ import annotations
+
+revision = "594f9e82b5d4"
+down_revision = ("0030_loyalty_reais_por_ponto", "0030_order_destination_coordinates")
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
